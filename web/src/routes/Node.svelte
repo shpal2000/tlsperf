9a36b701b4b55af00dce0bf0fcaf1471a8dd43e4
tlspack <shpal360@gmail.com>
2022-01-17 01:25:09 +0000

    webui

--- web/src/routes/Node.svelte
+++ web/src/routes/Node.svelte
@@ -2,36 +2,49 @@
     import { nodeTreeRoot } from '../store';
     import { selectedNode } from '../store.js';
     import { navRoute } from '../store.js';
-
+    import NotFound from "./NotFound.svelte";
     import { onMount } from "svelte";
     
     export let params = {};
+    let nodeFound = true;
 
 
     onMount ( () => {
 
-        $selectedNode.ParentName = params.nodeGroupName;
-        $selectedNode.Name = params.nodeName;
-        $selectedNode.Type = 'Node';
-
-        $navRoute.Paths = ['Traffic Nodes', params.nodeGroupName, params.nodeName];
-        $navRoute.Views = [];
-        $navRoute.ViewSelect = 'SingleView';
-
+        let nodeSelected = null;
         // console.log($nodeTreeRoot.children);
-
         let nodeGroup = $nodeTreeRoot.children.find (ng => ng.Name==params.nodeGroupName);
-
         // console.log(nodeGroup);
-        nodeGroup.expanded = true;
+        if (nodeGroup) {
+            nodeSelected = nodeGroup.children.find (n => n.Name==params.nodeName);
+        }
+        if (!nodeSelected) {
+            nodeFound = false;
+        } else {
+            $selectedNode.ParentName = params.nodeGroupName;
+            $selectedNode.Name = params.nodeName;
+            $selectedNode.Type = 'Node';
 
-        $nodeTreeRoot.expanded = true;
-        $nodeTreeRoot.children = $nodeTreeRoot.children;
+            $navRoute.Paths = ['Traffic Nodes', params.nodeGroupName, params.nodeName];
+            $navRoute.Views = [];
+            $navRoute.ViewSelect = 'SingleView';
+
+
+            nodeGroup.expanded = true;
+
+            $nodeTreeRoot.expanded = true;
+            $nodeTreeRoot.children = $nodeTreeRoot.children;
+        }
     });
 
 </script>
+
+{#if nodeFound}
 <p>
     Node - {params.nodeName}
     <br/>
     NodeGroup - {params.nodeGroupName}
 </p>
+{:else}
+<NotFound />
+{/if}
