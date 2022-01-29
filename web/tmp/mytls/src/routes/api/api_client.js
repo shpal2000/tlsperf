5a3ffa27eef9edb245014400c9e1dabc98e81d2c
tlspack <shpal360@gmail.com>
2022-01-29 04:59:35 +0000

    webui

--- web/tmp/mytls/src/routes/api/api_client.js
+++ web/tmp/mytls/src/routes/api/api_client.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-console.log ('Api on '+ process.env.HOST + ':' + process.env.BPORT);
-
 const apiClient = axios.create ({
     baseURL: 'http://' + process.env.HOST + ':' + process.env.BPORT + '/api',
     headers: {
